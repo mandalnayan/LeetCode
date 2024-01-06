dcc11c6c046472f069c678d60834b12a86a6c44f
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-06 11:16:55 +0530

    Time: 1108 ms (8.14%), Space: 61.4 MB (40.17%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,7 +7,37 @@ class Solution {
         
         Arrays.sort(nums);
         
-        int ans = 1, curLen = 1;
+        
+//      Optimal Solution using HashMap
+        
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) 
+            if (!map.containsKey(nums[i])) map.put(nums[i], true);
+        
+        int ans = 1, i = 0;
+        while (i < n) {
+            
+            int CV = nums[i];
+            
+//       checking is it's previous value present, if not then curent value is starting point
+            if (!map.containsKey(CV - 1)) {
+                int CL = 1;
+                
+                while (map.containsKey(CL + CV)) CL++;
+                
+                ans = Math.max(CL, ans);
+            }
+            i++;
+        }
+        
+        return ans;
+    }
+}
+        
+        
+        /*
+        // Better solution using "Sorting" | Time Complexity : O(nlogn) | Space Complexity : O(1)
         
         for (int i = 0; i < n-1; i++) {
             int cur =  nums[i];
@@ -26,3 +56,4 @@ class Solution {
         return Math.max(curLen, ans);
     }
 }
+*/
