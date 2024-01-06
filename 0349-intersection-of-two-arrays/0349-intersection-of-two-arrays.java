2ed84695fdc8c72cd839dc5e66c88c8cc397199b
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-06 14:11:29 +0530

    Time: 3 ms (53.37%), Space: 43.1 MB (69.93%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -6,8 +6,35 @@ class Solution {
         
         ArrayList<Integer> ls = new ArrayList<>();
         
+//  Better solution using Sorting  => Time Complexity : O(nlogn) | Space Complexity : O(1)
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i1 = 0, i2 = 0;
+        while (i1 < n1 && i2 < n2) {            
+            if(i1 > 0 && nums1[i1] == nums1[i1-1]) {
+                i1++;
+                continue;
+            }
+            else if (nums1[i1] == nums2[i2] ) {
+                ls.add(nums1[i1]);
+                i1++;
+                i2++;
+            }else if(nums1[i1] < nums2[i2]) i1++;
+            else i2++;
+        }
+        
+        int ans[] = new int[ls.size()];
+        int i = 0;        
+        while (i < ls.size()) ans[i] = ls.get(i++);
+        
+        return ans; 
+    }
+}
+/*
+// Brute force approach using nested loop; => Time Complexity : O(nlogn) | Space Complexity : O(1)
         for (int i = 0; i < n1; i++) {
-            
         for (int j = 0; j < n2; j++){
             if (nums1[i] == nums2[j]){
                  if (nums2[j] != -1 && !ls.contains(nums2[j]))
@@ -27,3 +54,4 @@ class Solution {
         
     }
 }
+*/
