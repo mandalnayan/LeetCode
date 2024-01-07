791e46614f56ed196177fafedccd23c44b9767bd
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-07 14:13:33 +0530

    Time: 1 ms (99.11%), Space: 45.2 MB (29.63%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,15 +4,17 @@ class Solution {
 //      Base case
         if (n == 1) return 0;
         
+//       Finding right sum
+        int rs = 0;
+        for (int j = 1; j < n; j++) rs += nums[j];
+        
         int ls = 0;
-        for (int i = 0; i < n; i++) {
-//             Finding right sum
-            int rs = 0;
-            for (int j = i+1; j < n; j++) rs += nums[j];
+        for (int i = 0; i < n-1; i++) {
             if (ls == rs) return i;
             ls += nums[i];
+            rs -= nums[i+1];
         }
-        
+        if (ls == rs) return n-1;
         return -1;
     }
 }
