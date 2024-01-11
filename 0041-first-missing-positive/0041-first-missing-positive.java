6f730d7c1ac69b49b606590dad17c5d9427824d7
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-11 17:57:33 +0530

    Time: 10 ms (28.29%), Space: 58.2 MB (57.87%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,6 +2,28 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         
+        // Approach -1  Using HashMap
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        
+        int i = 0;
+        while (i < n) {
+            if (nums[i] > 0) map.put(nums[i], true);
+            i++;
+        }
+        int mp = 1;
+        while (true) {
+            if (!map.containsKey(mp)) break;
+            mp++;
+        }
+        
+        return mp;
+
+    }
+}
+
+        
+        /*
+        // Using sorting , Time Complexit : O(nlogn) | Space Complexity : O(1)
         Arrays.sort(nums);
         int e = 1, i = 0;
         while (i < n) {
@@ -12,3 +34,4 @@ class Solution {
         return e;        
     }
 }
+*/
