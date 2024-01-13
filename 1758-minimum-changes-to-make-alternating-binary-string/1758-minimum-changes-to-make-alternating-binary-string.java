b662543712fdfa591e203094f384d4353a0dadde
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-13 13:33:15 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (71.61%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -5,23 +5,18 @@ class Solution {
         if (n == 1) return 0;
         
     // Converting string to  array
-        int opr1 = convert(s.toCharArray(), n, 0);
-        
-        char ch[] = s.toCharArray(); 
-        ch[0] = ch[0] == '0' ? '1' : '0';
-        int opr2 = convert(ch, n, 1);   
+        int opr1 = convert(s, n, '1', '0');
+        int opr2 = convert(s, n, '0', '1');   
         
         return Math.min(opr1, opr2);      
      }
     
-    int convert(char ch[], int n, int opr){
-        int i = 1;
+    int convert(String s, int n, char odd, char even){
+        int i = 0, opr = 0;
         while(i < n) {
-            if (ch[i-1] == ch[i]) {
-                char c = ch[i];
-                ch[i] = c =='1'? '0' : '1';
-                opr++;
-            }
+            char  ch  =  s.charAt(i);
+            if (i %2 == 0  && ch != even)  opr++;
+            else if(i % 2 == 1 && ch != odd) opr++;  
             i++;
         }
         return opr; 
