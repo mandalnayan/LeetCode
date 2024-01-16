463f04e2af9f9d7126cd97cbdeb4f6167b083944
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-16 10:32:08 +0530

    Time: 1 ms (81.32%), Space: 41.3 MB (39.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,20 +6,20 @@ class Solution {
       //Base case-1
       if (n == 1) return strs[0];
       
-      String LCP = "";   // Longest_Common_Prefix
+      StringBuilder LCP = new StringBuilder();   // Longest_Common_Prefix
       int i = 0, len = 0;
       String fs = strs[i];
       while(i < fs.length()) {              
            char ch = fs.charAt(i);
            int j = 1;
            while (j < n) {
-             if (strs[j].length() <= i ||                              strs[j].charAt(i) != ch) return LCP;
+             if (strs[j].length() <= i ||                              strs[j].charAt(i) != ch) return LCP.toString();
              j++;
            }
-          LCP += ch;
+          LCP.append(ch);
           len++;
           i++;
       }
-      return LCP;
+      return LCP.toString();
     }
 }
