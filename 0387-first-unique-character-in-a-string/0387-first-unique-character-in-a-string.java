f55eca6443f0d6effe51757b7637fb60555d660e
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-20 18:07:09 +0530

    Time: 22 ms (54.43%), Space: 44.5 MB (48.60%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -2,23 +2,13 @@ class Solution {
     public int firstUniqChar(String s) {
       int n = s.length();
       if (n == 1) return 0;
-      
-     
-      
-      int arr[] = new int[26];
-      
+      // indexOf(char) return first index of alphabet && lastIndexOf(char) return last index of character, else return -1 if character not present
       int i = 0;
       while(i < n) {
-        arr[s.charAt(i++)-'a']++;
+          char ch = s.charAt(i);
+          if (s.indexOf(ch) == s.lastIndexOf(ch)) return i;
+          i++;
       }
-      
-      // Finding the first non-repeating character
-      i = 0;
-      while(i < n) {
-        if (arr[s.charAt(i)-'a'] == 1) return i;
-        i++;
-      }
-      
       return -1;
     }
 }
