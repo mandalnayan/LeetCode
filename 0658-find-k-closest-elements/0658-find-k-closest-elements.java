945043a4b6c55bb02e839d1349d82ce1b67bb251
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-01-30 22:50:34 +0530

    Time: 4 ms (91.40%), Space: 46.1 MB (26.46%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,36 +1,27 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
       int n = arr.length;
-      // Base case
-    //  if (k == n) return 
-      
-      PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-      int i = 0;
-      while(i < n) {
-        if (arr[i] >= x) break;
-        pq.add(arr[i]);
-        i++;
+      List<Integer> resLs = new ArrayList<>();
+      int idx = search(arr, n-k, x);
+      int r = idx, l = idx-1;
+      // expand the left and right window
+      while(k-- > 0) {
+          if (l < 0 || (r < n && arr[r] - x < x-arr[l])) r++;
+          else l--;
       }
-      List<Integer> resultList = new ArrayList<>();
-      while(resultList.size() != k && !pq.isEmpty()) {
-        int val = pq.peek();
-        if (i < n) {
-          int b = arr[i];
-          int res1 = Math.abs(val - x);
-          int res2 = Math.abs(b-x);
-          if (res1 > res2 || (res1 == res2 && val > b)) {
-              val = b;
-              i++;
-          }
+      l++;
+      while(l < r) resLs.add(arr[l++]);
+      return resLs; 
+    }
+    int search(int arr[], int n, int x) {
+        int l = 0, r = n;
+        while(l <= r) {
+            int m = (l+r)/2;
+            if (arr[m] == x) return m;
+            else if (arr[m] < x) l = m+1;
+            else r = m-1;
         }
-         resultList.add(val);
-         if (val == pq.peek()) pq.remove();
-        }
-      
-      while(resultList.size() < k && i < n) 
-               resultList.add(arr[i++]);
-      Collections.sort(resultList);
-      return resultList; 
+        return l;
     }
 
 }
