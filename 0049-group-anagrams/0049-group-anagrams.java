8e255e3ec68bf222f0d7115f02bf93529d13f668
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-02-06 10:23:24 +0530

    Time: 48 ms (8.54%), Space: 51.7 MB (5.32%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,40 +1,41 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res_ls = new ArrayList<>();
-        int n = strs.length;
-        //Base case
-        if(n == 1) {
-            List<String> ls = new ArrayList<>();
-            ls.add(strs[0]);
-            res_ls.add(ls);
-            return res_ls;
-        } 
-        Map<Map<Character, Integer>, List<String>> map_list = new HashMap<>();
-        int i =  0;
-        while(i < n) {
-            Map<Character, Integer> map = convert_map(strs[i]);
-            if(!map_list.containsKey(map)) {
-                List<String> ls = new ArrayList<>();
-                ls.add(strs[i]);
-                map_list.put(map, ls);
-            } else {
-             map_list.get(map).add(strs[i]);
-            }
-            i++;
+      int n = strs.length;
+      List<List<String>> resList = new ArrayList<>();
+      
+      if (n == 1) {
+        List<String> ls = new ArrayList<>();
+        ls.add(strs[0]);
+        resList.add(ls);
+        return resList;
+      }
+      
+      HashMap<Map<Character, Integer>, List<String>> listMap = new HashMap<>();
+      int i = 0;
+      while (i < n) {
+        String curStr = strs[i++];
+        Map<Character, Integer> map = mapping(curStr);
+        List<String> ls = listMap.getOrDefault(map, new ArrayList<String>());
+        ls.add(curStr);
+        listMap.put(map, ls);
+        /*
+        if (listMap.containsKey(map)) {
+           listMap.put(map,  ls);
+        } else {
+          listMap.put(map, new)
         }
-        for (List<String> ls : map_list.values()){
-               res_ls.add(ls);
-        }
-        return res_ls;
+        */
+      }
+      for(List<String> ls : listMap.values()) {
+        resList.add(ls);
+      }
+      return resList;      
     }
-
-    public Map<Character, Integer> convert_map(String s) {
-        Map<Character, Integer> map =  new HashMap<>();
-        int i = 0, n = s.length();
-        while(i < n) {  
-            char ch = s.charAt(i++);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        return map;
+  public Map<Character, Integer> mapping(String str) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char ch : str.toCharArray()) {
+         map.put(ch, map.getOrDefault(ch, 0)+1);
     }
+    return map;
+  }
 }
