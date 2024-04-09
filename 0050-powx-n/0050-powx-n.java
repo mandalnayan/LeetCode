5131d2e793b032d1d314628edfc818bcbfe0cfdc
Nayan Mandal <121637927+Nayan417@users.noreply.github.com>
2024-04-09 11:11:00 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (26.40%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-     return Math.pow(x, n);   
+     // Base case
+      if (x == 1 || n == 1) return x;
+       
+      if (n < 0) {
+        n = -n;
+        x = 1 / x;
+      }
+      
+      double pow = 1;
+      while (n != 0) {
+           if ((n & 1) != 0) {
+            pow *= x;
+           }
+           x *= x;
+           n = n >>> 1;
+      }
+      
+     return  pow;   
     }
 }
